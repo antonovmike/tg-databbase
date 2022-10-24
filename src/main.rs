3927aef952c8e7e9ccbddade395a61af7bd833bc
antonovmike <antonvma@outlook.com>
2022-10-24 10:30:58 +0500

    sync works

--- src/main.rs
+++ src/main.rs
@@ -49,38 +49,15 @@ async fn echo(api: Ref<Api>, chat_id: ChatId, message: Message) -> Result<(), Ex
         "/dbadd" => api.execute(SendMessage::new(chat_id.clone(), "Received /dbadd")).await?,
         "/print" => api.execute(
             SendMessage::new(
-                chat_id.clone(), format!("{:?}", sorted(second_part)
-                )
+                chat_id.clone(), format!("{}", some(second_part))
             )).await?,
         _ => api.execute(SendMessage::new(chat_id.clone(), "Error")).await?,
     };
+    
     Ok(())
 }
 
-// fn some(slice: &str) -> String {
-//     // "fn some".to_string()
-//     format!("fn some: {}", slice)
-// }
-
-fn sorted(slice: &str) -> Result<(), Error> {
-    let mut client = Client::connect(
-        "postgresql://dboperator:operatorpass123@localhost:5243/postgres", NoTls
-    )?;
-    
-    for row in client.query 
-    ("SELECT item, COUNT(item) AS goods 
-    FROM b_store GROUP BY id ORDER BY goods DESC", &[])? {
-        let (item, price): (Option<String>, Option<i64>) = (row.get(0), row.get(1));
-        
-        if item.is_some() && price.is_some() {
-            let sorted_db = Sorted {
-                item: item.unwrap(),
-                price: price.unwrap(),
-        };
-            println!("{} {}", sorted_db.item, sorted_db.price);
-            
-        }
-    }
-    
-    Ok(())
+fn some(slice: &str) -> String {
+    // "fn some".to_string()
+    format!("fn some: {}", slice)
 }
